e93935ebd94feaefd2c74d3ca9db27932a2e5c7c
Lee Lavy <lilodead@gmail.com>
2020-05-10 21:16:18 +0300

    Initial Commit

---
+++ MuseBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuseBox
+//
+//  Created by admin on 10/05/2020.
+//  Copyright © 2020 Lee Lavy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
