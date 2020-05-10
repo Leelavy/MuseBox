f94001a872dfa8a872b497a346c4550c74222b81
Lee Lavy <lilodead@gmail.com>
2020-05-10 22:02:33 +0300

    Check for additional errors in committing to GitHub

--- MuseBox/ViewController.swift
+++ MuseBox/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
     
     @IBAction func allSetButton(_ sender: Any) {
         self.titleLabel.text = "MUSEBOX IS SET!"
+        self.logoImageView.image = UIImage(named: "MuseboxTempLogo")
     }
     
     override func viewDidLoad() {
